07a28dcdb4eea3621cc181daae1bef135589e8a8
Alex L <xpress16@yandex.ru>
2024-06-05 13:42:42 +0300

    fixed types with no motorbikes

--- firstTask/motorbikes.sql
+++ firstTask/motorbikes.sql
@@ -17,7 +17,8 @@ CREATE TABLE motorbikes (
 -- insert
 INSERT INTO motorbikes_types (name) VALUES ('cruiser');
 INSERT INTO motorbikes_types (name) VALUES ('scooter');
-
+INSERT INTO motorbikes_types (name) VALUES ('pitbike');
+ 
 INSERT INTO motorbikes (name, is_discontinued, motorbikes_types_id) VALUES ('BMW', 0, 1);
 INSERT INTO motorbikes (name, is_discontinued, motorbikes_types_id) VALUES ('Mercedes', 0, 1);
 INSERT INTO motorbikes (name, is_discontinued, motorbikes_types_id) VALUES ('Java', 1, 1);
@@ -25,8 +26,17 @@ INSERT INTO motorbikes (name, is_discontinued, motorbikes_types_id) VALUES ('Hon
 INSERT INTO motorbikes (name, is_discontinued, motorbikes_types_id) VALUES ('Suzuki', 0, 2);
 
 -- fetch 
-SELECT mt.name, COUNT(m.id) AS num_bikes
-     FROM motorbikes m 
-LEFT JOIN motorbikes_types mt ON m.motorbikes_types_id = mt.id
-where m.is_discontinued = 0
-GROUP BY mt.name
+-- SELECT mt.name, COUNT(motorbikes_types_id) AS num_bikes
+--     FROM motorbikes_types mt
+-- LEFT JOIN motorbikes m ON m.motorbikes_types_id = mt.id
+-- where m.is_discontinued = 0
+-- GROUP BY mt.name;
+
+SELECT
+  mt.name,
+  COUNT(m.id)
+FROM motorbikes_types mt
+LEFT JOIN motorbikes m
+  ON m.motorbikes_types_id = mt.id
+  AND m.is_discontinued = 0
+GROUP BY mt.name;
